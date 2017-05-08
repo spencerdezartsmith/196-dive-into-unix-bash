faad74499521eb1276c4a7e44170b54844dd3a37
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-05-08 13:13:30 -0700

    Finish chapter 1

---
+++ command-line/exercise1-2.sh
@@ -0,0 +1,6 @@
+# Write a command that prints out the string “hello, world”. Extra credit: As in Listing 1, do it two different ways, both with and without using quotation marks.
+echo hello world
+echo '"hello world"'
+
+# Type the command echo ’hello (with a mismatched single quote), and then get out of trouble using the technique from Box 4.
+echo 'hello
---
+++ command-line/exercise1-6.sh
@@ -0,0 +1,9 @@
+# Write a command to print the string Use "man echo", including the quotes; i.e., take care not to print out Use man echo instead. Hint: Use double quotes in the inner string, and wrap the whole thing in single quotes.
+echo 'Use "man echo"'
+
+# By running man sleep, figure out how to make the terminal “sleep” for 5 seconds, and execute the command to do so.
+sleep 5;
+
+# Execute the command to sleep for 5000 seconds, realize that’s well over an hour, and then use the instructions from Box 4 to get out of trouble.
+sleep 5000;
+CMD C
