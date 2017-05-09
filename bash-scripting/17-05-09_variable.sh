d33e97c70e8b524850b5d2fd39940f42c89e6a8e
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-05-09 15:49:42 -0700

    Create bash script that gathers information from user

--- bash-scripting/17-05-09_variable.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "There were $# arguments passed into $0"
-echo
-echo "The second argument was $2"
---
+++ bash-scripting/gather_info.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Who am I talking to today?"
+read name
+echo "It is a pleasure to meet you, $name"
+echo "Please create a username and password"
+read -p "Username: " username
+read -sp "Password: " Password
+echo
+echo "Thank you $name. We have your username set to $username"
