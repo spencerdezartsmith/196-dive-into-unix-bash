58212b3b30bd5a24626f6d3c93d947bf18580243
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-05-10 14:57:13 -0700

    Build a check for file existence script

---
+++ bash-scripting/check_for_file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Take a one arg: filename and report whether that file exists
+
+if [ $# -eq 1 ]
+then
+	if [ -e $1 ]
+  then
+		echo "$1 exists!"
+	else
+		echo "$1 does not exist!"
+	fi
+else
+	echo "Usage: will check for the existence of a file. Enter one filename."
+fi
