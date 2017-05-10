9a7d9dd43586e91cc4f6abf81ed84d23d0777cd1
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-05-10 14:16:05 -0700

    Add file test parameter to print_contents script

--- bash-scripting/print_contents.sh
+++ bash-scripting/print_contents.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Write a Bash script which will take a single command line argument (a directory) and will print each entry in that directory. If the entry is a file it will print it's size. If the entry is a directory it will print how many items are in that directory.
 
-if [[ "$1" =~ [\.\w{2,3}] ]]
+if [ -f $1 ]
 then
   ls -lah $1 | awk '{print "This is a file of size " $5}'
 else
